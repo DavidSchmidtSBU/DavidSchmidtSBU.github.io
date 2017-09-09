d13bed22c28c9da9879b690019bc604805781e19
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2017-09-09 13:13:24 -0500

    Add files via upload

---
+++ TemperatureConversion.java
@@ -0,0 +1,28 @@
+package temperatureConversion;
+/* Temperature Conversion
+
+*/
+
+public class TemperatureConversion
+{
+	public static void main( String [] args )
+    {
+			final double FREEZING_POINT = 32;
+			final double VALUE1 = 5.0/9.0;
+			final double VALUE2 = 9.0/5.0;
+	int templnF = 212;
+	int templnC = 100;
+	
+	templnF = (int)((9 * templnC)/5) + 32;
+	System.out.println(templnF);
+			
+			
+					
+		
+		
+		
+    }
+		
+}
+    
+	
