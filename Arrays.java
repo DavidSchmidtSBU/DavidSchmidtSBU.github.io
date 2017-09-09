44756b97ac9c56e376cfe36e3e89bf5ec6817c8c
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2017-09-09 13:00:56 -0500

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+//CIS1144, Lab 8 
+//Name: David Schmidt
+//Date: 10-11-16
+//Filename: ArrayTest.java 
+//This program will experiment with arrays
+package arrays;
+ public class Arrays 
+ { 
+    public static void main (String [] args) 
+    { 
+     int [ ] a = { 3, 6, 10 };
+     for ( int i = 0; i < a.length; i++ )
+     {
+    	 System.out.println( "a[" + i + "] = " + a[i] );
+     }
+     
+    }//END main 
+ }//END Arrays
